c95871e3ebdc3d3619c33386d0b544b0f0d44533
Santiago Leal <slealnov@my.centennialcollege.ca>
2022-11-15 02:17:37 -0500

    added mongo atlas config

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+//export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://30193542:leal1234@comp229.etmalwn.mongodb.net/media1?retryWrites=true&w=majority"
